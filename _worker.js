dacd438a516e5abeaefdb04617956a3d6b48b1f0
ccui03 <64187400+ccui03@users.noreply.github.com>
2024-08-29 18:12:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,8 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '68809c72-b917-490c-b8cd-e07dfe99fc2a';
 // proxyIP主要是为了转发请求到部署在CF上的网站使用的，可以使用以下域名
-const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
+//const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.bestip.one'];
+let proxyIPs = '';
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
